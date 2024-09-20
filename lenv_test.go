0239a3e540ba79a888bc767879e405e423c9d751
Ty Hopp <hopp.ty.c@gmail.com>
2024-09-20 14:42:16 +0800

    test: check, utility functions

---
+++ lenv_test.go
@@ -0,0 +1,162 @@
+package lenv
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+	teardown()
+	os.Exit(code)
+}
+
+func teardown() {
+	os.Remove(".env")
+	os.Remove(".env2")
+	os.Remove(".lenv")
+	os.Remove("testdata/.env")
+}
+
+func TestGetEnvFilePath(t *testing.T) {
+	tmpEnvFile, err := os.Create(".env")
+	if err != nil {
+		t.Fatalf("failed to create temp .env file: %v", err)
+	}
+	defer os.Remove(tmpEnvFile.Name())
+
+	path, err := GetEnvFilePath()
+	fmt.Printf("path: %s\n", path)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if path == "" {
+		t.Error("expected .env file path to exist")
+	}
+}
+
+func TestGetEnvFilePath_FileDoesNotExist(t *testing.T) {
+	_, err := os.Stat(".env")
+	if err == nil {
+		t.Fatal(".env file should not exist before the test")
+	}
+
+	path, err := GetEnvFilePath()
+	if err == nil {
+		t.Error("expected an error when .env file does not exist")
+	}
+	if path != "" {
+		t.Error("expected .env file path to be empty when file does not exist")
+	}
+}
+
+func TestReadLenvFile(t *testing.T) {
+	tmpLenvFile, err := os.Create(".lenv")
+	if err != nil {
+		t.Fatalf("failed to create temp .lenv file: %v", err)
+	}
+	expectedDestinations := []string{"a", "b"}
+	tmpLenvFile.WriteString(strings.Join(expectedDestinations, "\n"))
+	defer os.Remove(tmpLenvFile.Name())
+
+	destinations, err := ReadLenvFile()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if reflect.DeepEqual(destinations, expectedDestinations) {
+		t.Error("expected .lenv file content to match the expected destinations")
+	}
+}
+
+func TestReadLenvFile_FileDoesNotExist(t *testing.T) {
+	_, err := os.Stat(".lenv")
+	if err == nil {
+		t.Fatal(".lenv file should not exist before the test")
+	}
+
+	destinations, err := ReadLenvFile()
+	if err == nil {
+		t.Error("expected an error when .lenv file does not exist")
+	}
+	if !reflect.DeepEqual(destinations, []string{}) {
+		t.Error("expected .env file path to be empty when file does not exist")
+	}
+}
+
+func TestCheck(t *testing.T) {
+	source := ".env"
+	destinations := []string{"a", "b"}
+
+	tmpEnvFile, err := os.Create(source)
+	if err != nil {
+		t.Fatalf("failed to create temp .env file: %v", err)
+	}
+	defer os.Remove(tmpEnvFile.Name())
+
+	for _, dest := range destinations {
+		err := os.Symlink(source, dest)
+		if err != nil {
+			t.Fatalf("failed to create symlink: %v", err)
+		}
+		defer os.Remove(dest)
+	}
+
+	err = Check(source, destinations)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
+
+func TestCheck_SymlinkPointsToDifferentSource(t *testing.T) {
+	source := ".env"
+	destinations := []string{"testdata/.env"}
+
+	tmpEnvFile, err := os.Create(source)
+	if err != nil {
+		t.Fatalf("failed to create temp .env file: %v", err)
+	}
+	defer os.Remove(tmpEnvFile.Name())
+
+	differentSource := ".env2"
+	tmpEnvFile2, err := os.Create(differentSource)
+	if err != nil {
+		t.Fatalf("failed to create temp .env2 file: %v", err)
+	}
+	defer os.Remove(tmpEnvFile2.Name())
+
+	err = os.Symlink(differentSource, destinations[0])
+	if err != nil {
+		t.Fatalf("failed to create symlink: %v", err)
+	}
+	defer os.Remove(destinations[0])
+
+	err = Check(source, destinations)
+	if err == nil {
+		t.Error("expected an error when symlink points to a different source")
+	}
+}
+
+func TestCheck_PhysicalFileExists(t *testing.T) {
+	source := ".env"
+	destinations := []string{"testdata/.env"}
+
+	tmpEnvFile, err := os.Create(source)
+	if err != nil {
+		t.Fatalf("failed to create temp .env file: %v", err)
+	}
+	defer os.Remove(tmpEnvFile.Name())
+
+	tmpDestFile, err := os.Create(destinations[0])
+	if err != nil {
+		t.Fatalf("failed to create temp destination file: %v", err)
+	}
+	defer os.Remove(tmpDestFile.Name())
+
+	err = Check(source, destinations)
+	if err == nil {
+		t.Error("expected an error when physical file exists at destination")
+	}
+}
