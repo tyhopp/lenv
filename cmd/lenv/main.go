fb82cfb21d8e4be9b8c53085d19d7e0e6adbfbee
Ty Hopp <hopp.ty.c@gmail.com>
2024-09-20 16:16:58 +0800

    feat: handle subcommands in main

--- cmd/lenv/main.go
+++ cmd/lenv/main.go
@@ -10,18 +10,44 @@ import (
 func main() {
 	logger := log.New(os.Stdout, "", 0)
 
-	source, err := lenv.GetEnvFilePath()
-	if err != nil {
-		logger.Fatal(err)
+	if len(os.Args) < 2 {
+		logger.Fatal("lenv: missing subcommand, available: check, link, unlink")
 	}
 
-	destinations, err := lenv.ReadLenvFile()
-	if err != nil {
-		logger.Fatal(err)
+	getPaths := func() (string, []string) {
+		source, err := lenv.GetEnvFilePath()
+		if err != nil {
+			logger.Fatal(err)
+		}
+
+		destinations, err := lenv.ReadLenvFile()
+		if err != nil {
+			logger.Fatal(err)
+		}
+
+		return source, destinations
 	}
 
-	err = lenv.Check(source, destinations)
-	if err != nil {
-		logger.Fatal(err)
+	switch os.Args[1] {
+	case "check":
+		source, destinations := getPaths()
+		err := lenv.Check(source, destinations)
+		if err != nil {
+			logger.Fatal(err)
+		}
+	case "link":
+		source, destinations := getPaths()
+		err := lenv.Link(source, destinations)
+		if err != nil {
+			logger.Fatal(err)
+		}
+	case "unlink":
+		_, destinations := getPaths()
+		err := lenv.Unlink(destinations)
+		if err != nil {
+			logger.Fatal(err)
+		}
+	default:
+		logger.Fatal("lenv: unknown subcommand, available: check, link, unlink")
 	}
 }
